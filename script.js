6475e9534b737d90a50c6ff5fc72e69e06c206c0
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-24 07:58:08 -0600

    minor changes

--- script.js
+++ script.js
@@ -23,28 +23,20 @@ element.innerHTML = phrase;
 
 */
 
+
 // 1.Paste the code below into your HTML file.
 // 2.Take the contents of the sonnet div and place it in a variable
 // 3.Find and output the starting position of the word "orphans"
 //  401
-// 4.Output the number of characters in the sonnet
-//  618
-// 5.Replace the first occurance of the string "winter" with "yuletide"
-//
-// 6.Replace all occurances of the string "the" with "a large"
-//
-// 7.Set the content of the sonnet div with the new string
-
-
-
-
 var excercise = document.getElementById("sonnet").innerHTML;
 console.log(excercise.indexOf("orphans"));
-
+// 4.Output the number of characters in the sonnet
+//  618
 console.log(excercise.length);
-
+// 5.Replace the first occurance of the string "winter" with "yuletide"
 var newSonnet = excercise.replace("winter", "yuletide");
 console.log(newSonnet);
-
+// 6.Replace all occurances of the string "the" with "a large"
 var newerSonnet = newSonnet.replace(/the/g, 'a large');
 console.log(newerSonnet);
+// 7.Set the content of the sonnet div with the new string
