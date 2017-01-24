275ec6a708809c84f57a1fef68d573514b8bf1b0
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-24 07:45:27 -0600

    finishing excercise

---
+++ script.js
@@ -0,0 +1,50 @@
+/*
+
+var quote = "\"we jowj foe jwpo jpojw. eozj ojeoj wpojwpoj w\"";
+
+console.log(quote.length);
+
+console.log(quote.indexOf("z"));
+
+console.log(quote[6]);
+
+var position = quote.charAt(12);
+console.log("Letter at spot 12 ", position);
+
+var newPhrase = quote.replace("jowj", "balls");
+console.log(newPhrase);
+
+
+
+
+var phrase = "Hello World.";
+var element = document.getElementById("container");
+element.innerHTML = phrase;
+
+*/
+
+// 1.Paste the code below into your HTML file.
+// 2.Take the contents of the sonnet div and place it in a variable
+// 3.Find and output the starting position of the word "orphans"
+//  401
+// 4.Output the number of characters in the sonnet
+//  618
+// 5.Replace the first occurance of the string "winter" with "yuletide"
+//
+// 6.Replace all occurances of the string "the" with "a large"
+//
+// 7.Set the content of the sonnet div with the new string
+
+
+
+
+var excercise = document.getElementById("sonnet").innerHTML;
+console.log(excercise.indexOf("orphans"));
+
+console.log(excercise.length);
+
+var newSonnet = excercise.replace("winter", "yuletide");
+console.log(newSonnet);
+
+var newerSonnet = newSonnet.replace(/the/g, 'a large');
+console.log(newerSonnet);
