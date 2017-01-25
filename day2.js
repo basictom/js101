81402991ad7189a90fd8b446d551d181b8a1b730
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-24 20:49:06 -0600

    arrays

--- day2.js
+++ day2.js
@@ -1,6 +1,6 @@
-// Learning Switch Statements
+// SWITCHES
 
-var day ="Friday";
+/*var day ="Friday";
 
 switch(day){
   case "Monday":
@@ -18,3 +18,39 @@ switch(day){
       console.log("still working...");
     }
 }
+*/
+
+
+// ARRAYS
+
+var fruit = ['apples', 'bananans', 'melons', 'oranges', 'squash'];
+var fruitString = fruit.join(', ');
+console.log('fruitString', fruitString);
+
+var catColors = 'green,red,yellow,black';
+var catColorsArray = catColors.split(',');
+console.log(catColorsArray);
+
+var reverseFruit = fruit.reverse('');
+console.log(reverseFruit);
+
+var sortedFruit = fruit.sort('');
+console.log(sortedFruit);
+
+var numbers = [1,2,3,4,11,6,7,8,9];
+var numSort = numbers.sort(function(a,b){return a-b});
+console.log(numSort);
+
+var mixedCrap = ['a','b', 12, 16, 'ef'];
+console.log(mixedCrap.sort());
+
+console.log(fruit.indexOf('apples'));
+
+var sport = ['hockey', 'baseball', 'football', 'soccer', 'lacrosse'];
+var sportSlice = sport.slice(1, 2, 3);
+console.log(sportSlice);
+
+var colors = ['red', 'blue', 'grey', 'yellow'];
+//sort, reverse, make a string
+var finalAnswer = colors.sort().reverse().join(' cat ');
+console.log(finalAnswer);
