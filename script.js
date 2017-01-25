5809707b8467d708df373f34046657d2d15271b1
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-24 18:50:15 -0600

    deleting console.logs

--- script.js
+++ script.js
@@ -2,17 +2,10 @@
 
 var quote = "\"we jowj foe jwpo jpojw. eozj ojeoj wpojwpoj w\"";
 
-console.log(quote.length);
-
-console.log(quote.indexOf("z"));
-
-console.log(quote[6]);
 
 var position = quote.charAt(12);
-console.log("Letter at spot 12 ", position);
 
 var newPhrase = quote.replace("jowj", "balls");
-console.log(newPhrase);
 
 
 
@@ -32,14 +25,9 @@ var excercise = document.getElementById("sonnet").innerHTML;
 console.log(excercise.indexOf("orphans"));
 // 4.Output the number of characters in the sonnet
 //  618
-console.log(excercise.length);
 // 5.Replace the first occurance of the string "winter" with "yuletide"
 var newSonnet = excercise.replace("winter", "yuletide");
-console.log(newSonnet);
 // 6.Replace all occurances of the string "the" with "a large"
 var newerSonnet = newSonnet.replace(/the/g, 'a large');
-console.log(newerSonnet);
 // 7.Set the content of the sonnet div with the new string
-var sonnet = newerSonnet;
-var newContent = document.getElementById("sonnet");
-element.innerHTML = sonnet;
+newSonnet.innerHTML = newerSonnet;
