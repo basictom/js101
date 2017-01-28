f7ba39cfeee508626b0c8e62f1ef8ed3624868c9
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-28 10:09:37 -0600

    updating

--- day2.js
+++ day2.js
@@ -23,34 +23,54 @@ switch(day){
 
 // ARRAYS
 
-var fruit = ['apples', 'bananans', 'melons', 'oranges', 'squash'];
-var fruitString = fruit.join(', ');
-console.log('fruitString', fruitString);
+// var fruit = ['apples', 'bananans', 'melons', 'oranges', 'squash'];
+// var fruitString = fruit.join(', ');
+// console.log('fruitString', fruitString);
+//
+// var catColors = 'green,red,yellow,black';
+// var catColorsArray = catColors.split(',');
+// console.log(catColorsArray);
+//
+// var reverseFruit = fruit.reverse('');
+// console.log(reverseFruit);
+//
+// var sortedFruit = fruit.sort('');
+// console.log(sortedFruit);
+//
+// var numbers = [1,2,3,4,11,6,7,8,9];
+// var numSort = numbers.sort(function(a,b){return a-b});
+// console.log(numSort);
+//
+// var mixedCrap = ['a','b', 12, 16, 'ef'];
+// console.log(mixedCrap.sort());
+//
+// console.log(fruit.indexOf('apples'));
+//
+// var sport = ['hockey', 'baseball', 'football', 'soccer', 'lacrosse'];
+// var sportSlice = sport.slice(1, 2, 3);
+// console.log(sportSlice);
+//
+// var colors = ['red', 'blue', 'grey', 'yellow'];
+// //sort, reverse, make a string
+// var finalAnswer = colors.sort().reverse().join(' cat ');
+// console.log(finalAnswer);
 
-var catColors = 'green,red,yellow,black';
-var catColorsArray = catColors.split(',');
-console.log(catColorsArray);
 
-var reverseFruit = fruit.reverse('');
-console.log(reverseFruit);
 
-var sortedFruit = fruit.sort('');
-console.log(sortedFruit);
+for(var i=0; i<5; i++){
+  console.log(i);
+};
 
-var numbers = [1,2,3,4,11,6,7,8,9];
-var numSort = numbers.sort(function(a,b){return a-b});
-console.log(numSort);
 
-var mixedCrap = ['a','b', 12, 16, 'ef'];
-console.log(mixedCrap.sort());
+// write a for loop that increments by 10 each time = 10 times
+// 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+for(var k=0; k<101; k+=10){
+  console.log("k",k);
+}
 
-console.log(fruit.indexOf('apples'));
 
-var sport = ['hockey', 'baseball', 'football', 'soccer', 'lacrosse'];
-var sportSlice = sport.slice(1, 2, 3);
-console.log(sportSlice);
+// for loop that pushes the index to an array for 5 times
+// console.log (array) \\ [0,1,2,3,4]
 
-var colors = ['red', 'blue', 'grey', 'yellow'];
-//sort, reverse, make a string
-var finalAnswer = colors.sort().reverse().join(' cat ');
-console.log(finalAnswer);
+// for loop that starts at 100 and divides by 2 each time 3 times
+// 100, 50, 25,
